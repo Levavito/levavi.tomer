9405aab0d3f31ec501c8581889f1089841af6b38
Levavito <levavito1000@gmail.com>
2021-12-05 19:59:33 +0200

    Hw7

---
+++ arseni execise/lessons/app flask lesson 1/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about_func():  # put application's code here
+    return redirect(url_for('catalog_func'))
+
+
+@app.route('/catalog')
+def catalog_func():  # put application's code here
+    return 'catalog page!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
